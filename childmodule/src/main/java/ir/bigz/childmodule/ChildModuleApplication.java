ccba3b6297079c3cef659a259216c1382d8f96bc
pouyapouryaie <pouyapouryaie@gmail.com>
2021-12-08 07:26:13 +0330

    simple maven module

---
+++ childmodule/src/main/java/ir/bigz/childmodule/ChildModuleApplication.java
@@ -0,0 +1,11 @@
+package ir.bigz.childmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildModuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChildModuleApplication.class, args);
+    }
+}
